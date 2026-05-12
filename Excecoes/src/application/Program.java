2a02e4e16a945db56e0168d106907bc8530e1048
Adriel Felipe <adrielfelipevieira@gmail.com>
2026-05-12 00:18:23 -0300

    conceitos iniciais de tratamento de exceções com try-catch

---
+++ Excecoes/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position.");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error.");
+        }
+
+
+        System.out.println("End.");
+
+        sc.close();
+    }
+}
